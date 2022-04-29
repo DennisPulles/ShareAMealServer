8995e5493aa60a3f442f088ff96ca041c77161f7
DennisPulles <dennispulles@hotmail.com>
2022-04-29 09:56:00 +0200

    Update inmemdb.js

--- database/inmemdb.js
+++ database/inmemdb.js
@@ -2,5 +2,22 @@ const _userdb = []
 let id = 0
 
 module.exports = {
+
+    createUser(user, callback){
+        console.log('createUser called')
+        if (user && user.email && _userdb.filter((item) => item.email === user.email).length > 0){
+            const error = 'A user with this email already exists.'
+            console.log(error)
+            callback(error, undefined)
+        } else {
+            const userToAdd = {
+                id: id++,
+                ...user,
+            }
+            _userdb.push(userToAdd)
+            callback(undefined, userToAdd)
+        }
+    }
+
     
 }
