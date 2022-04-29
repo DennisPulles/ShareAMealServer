ea6da4739c8d165ad81d34e649e660983f543191
DennisPulles <dennispulles@hotmail.com>
2022-04-29 12:22:27 +0200

    db in get all users

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -9,26 +9,8 @@ var pool = mysql.createPool({
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE
 });
- 
-pool.getConnection(function(err, connection) {
-    if (err) throw err; // not connected!
-   
-    // Use the connection
-    connection.query('SELECT name, id FROM meal', function (error, results, fields) {
-      // When done with the connection, release it.
-      connection.release();
-   
-      // Handle error after the release.
-      if (error) throw error;
-   
-      // Don't use the connection here, it has been returned to the pool.
-      console.log('result = ', results)
 
-      pool.end((err)=> {
-        console.log('pool closed')
-      })
-    });
-});
+module.exports = pool
 
 pool.on('acquire', function (connection) {
     console.log('Connection %d acquired', connection.threadId);
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ const { title } = require('process');
 
 // let database = require('../../database/inmemdb')
 let database = []
+const dbconnection = require('../../database/dbconnection')
 let id = 0
 
 let controller = {
@@ -63,9 +64,25 @@ let controller = {
     },
 
     getAllUsers:(req, res)=>{
-        res.status(200).json({
-            status: 200,
-            result: database,
+        dbconnection.getConnection(function(err, connection) {
+            if (err) throw err; // not connected!
+           
+            // Use the connection
+            connection.query('SELECT name, id FROM meal', function (error, results, fields) {
+              // When done with the connection, release it.
+              connection.release();
+           
+              // Handle error after the release.
+              if (error) throw error;
+           
+              // Don't use the connection here, it has been returned to the dbconnection.
+              console.log('result = ', results)
+        
+              res.status(200).json({
+                  statusCode: 200,
+                  results: results
+              })
+            });
         });
     },
 
