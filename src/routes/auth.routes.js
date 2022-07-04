921adfa39840c37cf43e4ad93695be0106e5e459
DennisPulles <dennispulles@hotmail.com>
2022-07-04 13:41:05 +0200

    before each swap
    and usecase comments

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router()
 const AuthController = require('../controllers/auth.controller')
 
+//UC-101
 routes.post('/auth/login', AuthController.validateLogin, AuthController.login)
 
 module.exports = routes
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,16 +10,22 @@ router.get('/', (req, res) => {
 	})
 })
 
+//UC-201
 router.post('/api/user', userController.validateUser, userController.addUser)
 
-router.get('/api/user/profile',	authController.validateToken, userController.getUserProfileFromId)
-
-router.get('/api/user/:userId',	authController.validateToken, userController.getUserFromId)
-
+//UC-202
 router.get('/api/user', authController.validateToken, userController.getAllUsers)
 
+//UC-203
+router.get('/api/user/profile',	authController.validateToken, userController.getUserProfileFromId)
+
+//UC-204
+router.get('/api/user/:userId',	authController.validateToken, userController.getUserFromId)
+
+//UC-205
 router.put('/api/user/:userId',	authController.validateToken, userController.validateUpdateUser, userController.updateUserFromId)
 
+//UC-206
 router.delete('/api/user/:userId',	authController.validateToken, authController.validateOwnershipUser,	userController.deleteUserFromId)
 
 module.exports = router
